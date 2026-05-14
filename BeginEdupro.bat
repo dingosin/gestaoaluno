fd7ef1d80cb45779d4810f5a947c41a3cff7b94d
MALCOLM WALLACE <126926218+dingosin@users.noreply.github.com>
2026-05-14 18:00:14 -0300

    Add files via upload

--- BeginEdupro.bat
+++ BeginEdupro.bat
@@ -9,7 +9,7 @@ echo Iniciando servidor local...
 echo O programa abrira automaticamente no seu navegador.
 echo NAO FECHE ESTA JANELA ENQUANTO ESTIVER USANDO O SISTEMA.
 echo.
-
+call npm install
 :: Abre o navegador e inicia o servidor
 start http://localhost:3000
 call npm run dev
